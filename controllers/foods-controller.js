3069bc0f1c091128f27d6f7b3b49f2cd5f7f3d59
JNelson <nelson.jimmy@gmail.com>
2018-08-15 14:36:33 -0600

    Adds controller action, model method, and updates foods routes.

--- controllers/foods-controller.js
+++ controllers/foods-controller.js
@@ -6,6 +6,11 @@ class FoodsController {
     .then(foods => response.json(foods));
   }
 
+  static show(request, response, next) {
+    let food = Food.find(request.params.id)
+    .then(food => response.json(food));
+  }
+
 }
 
 module.exports = FoodsController;
