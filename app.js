213a105dd17bb470286d5faffb3a4f7d33235747
JNelson <nelson.jimmy@gmail.com>
2018-08-20 11:36:41 -0600

    Updates other configuration and seeds to help understand what values are being returned by the meals request.

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 var foodsRouter = require('./routes/api/v1/foods');
+var mealsRouter = require('./routes/api/v1/meals');
 
 var app = express();
 
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/foods', foodsRouter);
+app.use('/api/v1/meals', mealsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
