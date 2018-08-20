510a8aef17f321a6f0373fb0e4734afda12815b0
JNelson <nelson.jimmy@gmail.com>
2018-08-20 11:38:25 -0600

    Adds happy path test for finding a specified meal.

--- test/meals.get.spec.js
+++ test/meals.get.spec.js
@@ -14,12 +14,12 @@ const knex = require('knex')(configuration);
 describe("Meal GET endpoints", () => {
   beforeEach((done) => {
     knex.migrate.latest()
-    .then(() => {
-      knex.seed.run()
       .then(() => {
-        done();
+        knex.seed.run()
+          .then(() => {
+            done();
+          });
       });
-    });
   });
 
   afterEach((done) => {
@@ -37,8 +37,25 @@ describe("Meal GET endpoints", () => {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
         expect(res.body.length).to.eql(4);
+        expect(res.body[0].foods[0].name).to.eql("Banana");
+        expect(res.body[0].foods[0].calories).to.eql(150);
         done();
-      })
-    })
-  })
-})
+      });
+    });
+  });
+
+  describe("GET /meals/:id", () => {
+    it("should return the specified meal with its associated foods", (done) => {
+      chai.request(app)
+      .get("/api/v1/meals/4")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body.length).to.eql(1);
+        expect(res.body[0].name).to.eql("Dinner");
+        expect(res.body[0].foods).to.eql([]);
+        done();
+      });
+    });
+  });
+});
