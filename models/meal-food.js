4038d6b319d5635b52274607ca79c47b2862f5f2
JNelson <nelson.jimmy@gmail.com>
2018-08-20 21:29:02 -0600

    Adds route, controller action, and model method for destroying a meal food record.

--- models/meal-food.js
+++ models/meal-food.js
@@ -7,7 +7,13 @@ class MealFood {
   static create(params) {
     return database("meal_foods")
       .insert({ meal_id: params.meal_id, food_id: params.id })
-      .returning(['meal_id', 'food_id'])
+      .returning(['meal_id', 'food_id']);
+  }
+
+  static destroy(params) {
+    return database("meal_foods")
+      .where({ meal_id: params.meal_id, food_id: params.id })
+      .del();
   }
 }
 
