90e8ed814bbe635d319ff0c442e51b8fd93b88e2
JNelson <nelson.jimmy@gmail.com>
2018-09-04 16:12:00 -0600

    Adds routes for recipe search api

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const FoodsController = require('../../../controllers/foods-controller');
+const RecipesController = require('../../../controller/recipes-controller');
 
 router.get('/', FoodsController.index);
 router.get('/:id', FoodsController.show);
 router.post('/', FoodsController.create);
 router.put('/:id', FoodsController.update);
 router.delete('/:id', FoodsController.destroy);
+router.get('/:id/recipes', RecipesController.index);
 
 module.exports = router;
