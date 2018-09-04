98b4fddb7735e040bdca3ae079f5da08e1ce6944
JNelson <nelson.jimmy@gmail.com>
2018-09-04 16:34:31 -0600

    Updates method. Needs to handle incoming response better.

--- controllers/recipes-controller.js
+++ controllers/recipes-controller.js
@@ -1,13 +1,24 @@
 const Food = require('../models/food');
 const pry = require('pryjs');
+const fetch = require('node-fetch');
 
 class RecipesController {
   static index(request, response, next) {
     let food = Food.find(request.params.id)
     let food_name = food.name
     fetch(`http://api.yummly.com/v1/api/recipes?_app_id=4d31de00&_app_key=e660bdff4ade5b248a3ea3b64720dd8e&q=${food_name}`)
+    .then(response => response.json())
     .then((response) => {
-      response.matches.map
+      let matches = response.matches.map((match) => {
+        let recipe = {};
+        recipe.name = match['recipeName'];
+        recipe.url = match['smallImageUrls'][0];
+
+        return recipe
+      })
+      return matches
     })
   }
 }
+
+module.exports = RecipesController;
